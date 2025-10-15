5a9eb0b124d97e13caba7adfcceade29e3736c4b
Amir <qafariamirhossein@gmail.com>
2025-10-15 20:28:10 +0330

    fix layout

--- src/hoc/SectionWrapper.tsx
+++ src/hoc/SectionWrapper.tsx
@@ -16,7 +16,7 @@ const SectionWrapper = (
       <motion.section
         initial="hidden"
         whileInView="show"
-        viewport={{ once: true, amount: 0.25 }}
+        viewport={{ once: true, amount: 0.1 }}
         className={`${styles.padding} relative z-0 mx-auto max-w-7xl`}
         id={idName}
       >
